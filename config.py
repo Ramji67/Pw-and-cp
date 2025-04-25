e56c481f1dd9067fe6ec06a4886b2fb2c67be753
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2025-04-25 09:35:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "28088290"
 api_hash = "6998f2c585fdce65ac72dfa23d02b6ec"
-bot_token = "enter your bot token"
+bot_token = "7987579246:AAFehSclVqEUVfl0EDJz7v-0vXB6_Ig-JqY"
 auth_users = [5850397219,6373737837]
